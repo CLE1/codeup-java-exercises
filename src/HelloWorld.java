cf2a16f022b118c243bf32ccbc688dd9f19fc500
Christopher <espinoza.christopher117@gmail.com>
2021-06-22 09:42:59 -0500

    done

---
+++ src/HelloWorld.java
@@ -0,0 +1,47 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+
+        int myFavoriteNumber = 1;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "this string:";
+        myString += 3.1459;
+        System.out.println(myString);
+
+        var myNumber = 3.14;
+        System.out.println(myNumber);
+
+        var class = 1;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+
+
+
+    }
+}
+
+
+
+
---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,9 @@
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+        System.out.println((int)'h');
+
+    }
+
+}
