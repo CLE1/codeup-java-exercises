8c1cd8515acb5df8b5e5887fe6ea284455f93532
Christopher <espinoza.christopher117@gmail.com>
2021-06-27 22:16:18 -0500

    break

---
+++ src/PracticeEx.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class PracticeEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+//        String userOne = "Amado";
+//        String userTwo = "Erik";
+//        String userThree = "Moses";
+//
+//
+//        System.out.println("enter a name");
+//
+//        String userFour = sc.nextLine();
+
+//        int thisGuy = "Chris";
+//
+//        switch (thisGuy) {
+//            case "Erik" :
+//                System.out.println("Throw him in the trash");
+//            case "Amado":
+//                System.out.println("Throw him in the trash to");
+//                break;
+//            case "Chris":
+//                System.out.println("keep him");
+//                break;
+//            default:
+//                System.out.println("yeet");
+//                break;
+
+
+    }
+
+
+}
+
+//TODO: using previous mini exercise, allow the user to input a new username several times until a unique username is entered.
+
+
+//        MINI EXERCISE
+// TODO: create three unique usernames (user1, user2, user3).
+//  create a fourth user and set it equal to user input. (ask
+//  the user to enter a username). if the username has already
+//  been used by user1, user2, or user3, print "sorry this
+//  username already exists.
