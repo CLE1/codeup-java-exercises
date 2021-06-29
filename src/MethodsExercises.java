d63efb34540399a04687e0c19ef69683c07ba00b
Christopher <espinoza.christopher117@gmail.com>
2021-06-29 15:30:18 -0500

    break

---
+++ src/MethodsExercises.java
@@ -0,0 +1,13 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = a+b;
+        System.out.println(a+b);
+
+        int subtraction = 5-2;
+        System.out.println(subtraction);
+
+
+    }
+}
---
+++ src/PracticeMethods.java
@@ -0,0 +1,7 @@
+public class PracticeMethods {
+    public static void main(String[] args) {
+        System.out.println("Please tell us your age, name and if you have pets?");
+
+
+    }
+}
