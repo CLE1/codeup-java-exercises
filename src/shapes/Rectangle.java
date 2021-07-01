d09561c1471606f2dab4ffa9edc9a700fe9ee6a3
Christopher <espinoza.christopher117@gmail.com>
2021-07-01 16:33:43 -0500

    break

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,21 @@
 package shapes;
 
 public class Rectangle {
+
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public static int getArea(int length, int width){
+        return 2 * length + 2 * width;
+    }
+
+    public static int getPerimeter(int length, int width){
+        return length * width;
+    }
+
 }
