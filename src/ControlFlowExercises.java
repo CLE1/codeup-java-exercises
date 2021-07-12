a7e4f15a13e871bcd3292103ced9ff6d2022c591
Christopher <espinoza.christopher117@gmail.com>
2021-07-12 14:57:19 -0500

    break

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+       // ---------------
+
+//        int y = 0;
+//        do {
+//            System.out.println(y);
+//            y += 2;
+//        }while(y <= 100);
+
+        //-----------------
+
+
 
 
     }
