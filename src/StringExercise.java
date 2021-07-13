96cc310dc7df9d96cc3ef01b7d37ff2a689af5fe
Christopher <espinoza.christopher117@gmail.com>
2021-07-13 08:02:45 -0500

    break

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,7 +1,19 @@
 public class StringExercise {
     public static void main(String[] args) {
-        String message = "Yo";
-        System.out.println(message);
+        String PinkFloyd = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(PinkFloyd);
+
+        String random = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(random);
+
+        String drive = "In windows, the main drive is usually C:\\";
+        System.out.println(drive);
+
+        String slashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(slashes);
+
 
 
     }
