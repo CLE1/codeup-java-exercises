b965be709a12746bdd1f9c1d4cd5250ea59ffb3e
Christopher <espinoza.christopher117@gmail.com>
2021-07-14 09:33:29 -0500

    break

---
+++ src/warmup14JUL.java
@@ -0,0 +1,13 @@
+public class warmup14JUL {
+    public static void main(String[] args) {
+        String str = "I am Iron Man";
+
+        String[] strArray = str.split(" ");
+
+        System.out.println(str);
+
+        return;
+
+
+    }
+}
